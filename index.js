5fa566f3ce9b1a196bcf51e710647f3a2bcb0446
Kolja <decrier@ukr.net>
2024-08-26 09:24:04 +0200

    1.1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+require("dotenv").config();  // notwendig um das Modul dotenv nutzen zu können
+
+console.log('Host: ' + process.env.DATABASE_HOST);
+console.log('Database: ' + process.env.DATABASE_NAME);
+console.log('Username: ' + process.env.DATABASE_USER);
+console.log('Password: ' + process.env.DATABASE_PASSWORD);
+console.log('Port: ' + process.env.DATABASE_PORT);
